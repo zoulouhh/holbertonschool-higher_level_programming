3b0d9df36bc98dbb49453c4a7a8c2c508fc152b6
root <root@LAPTOP-JJUBT5E8.>
2025-05-12 14:18:18 +0200

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 str = "python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 str2 = str[39:67] + str[0:6] + str[107:112]
-print(str2)
-# The code above is a Python script that creates a string variable `str` containing a description of the Python programming language.
-# It then creates a new string `str2` by concatenating specific slices of `str`:
-# - `str[39:67]` extracts the substring "programming language"
-# - `str[0:6]` extracts the substring "python"
-# - `str[107:112]` extracts the substring "power" 
+print(str)
