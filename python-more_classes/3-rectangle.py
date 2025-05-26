21b4052d1d876b4d3972f8f9e75c42cfc71034d5
root <azladji.sil@gmail.com>
2025-05-26 01:20:35 +0000

    3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -59,4 +59,4 @@ class Rectangle:
             [rect.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
-        return "".join(rect
+        return "".join(rect)
