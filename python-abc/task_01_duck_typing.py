064002fe49f6a21b32080582e4ccb3961760bd21
root <root@LAPTOP-JJUBT5E8.>
2025-06-02 14:34:29 +0200

    task_01_duck_typing.py

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -2,32 +2,28 @@
 from abc import ABC, abstractmethod
 import math
 
-# Classe abstraite Shape
+
 class Shape(ABC):
     @abstractmethod
     def area(self):
-        """Calcule et retourne l'aire de la forme"""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """Calcule et retourne le périmètre de la forme"""
         pass
+
 
 class Circle(Shape):
     def __init__(self, radius):
-        if radius < 0:
-            raise ValueError("Radius cannot be negative")
         self.radius = radius
 
     def area(self):
-        return math.pi * self.radius ** 2
+        return math.pi * abs(self.radius) ** 2
 
     def perimeter(self):
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * abs(self.radius)
 
 
-# Classe concrète Rectangle
 class Rectangle(Shape):
     def __init__(self, width, height):
         self.width = width
@@ -39,8 +35,7 @@ class Rectangle(Shape):
     def perimeter(self):
         return 2 * (self.width + self.height)
 
-# Fonction shape_info utilisant le duck typing
-def shape_info(shape):
-    print("Area:", shape.area())
-    print("Perimeter:", shape.perimeter())
 
+def shape_info(shape):
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
