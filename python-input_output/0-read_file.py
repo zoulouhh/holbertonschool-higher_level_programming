a36b0d0bef22d0cfa18ae44434e708d505468cec
zoulouhh <azladji.sil@gmail.com>
2025-06-04 13:45:05 +0000

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+Executable script using the Python 3 interpreter.
+
+This shebang allows the script to be run directly from the terminal
+without needing to explicitly call 'python3'.
+"""
