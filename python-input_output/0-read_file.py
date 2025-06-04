fc7b3a2b144ee9ae73a5409cedfce303386e2762
zoulouhh <azladji.sil@gmail.com>
2025-06-04 13:46:32 +0000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,3 +5,6 @@ Executable script using the Python 3 interpreter.
 This shebang allows the script to be run directly from the terminal
 without needing to explicitly call 'python3'.
 """
+def read_file(filename=""):
+    """Read a UTF-8 text file and print its contents to
+    stdout without a trailing newline.
