830f3b3a796a97202ff6042b74cd7289d2ed3902
zoulouhh <azladji.sil@gmail.com>
2025-06-05 08:42:38 +0000

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,3 +5,10 @@ Executable script using the Python 3 interpreter.
 This script defines a function that writes a string to a UTF-8 encoded
 text file and returns the number of characters written.
 """
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF-8 encoded).
+
+    If the file does not exist, it will be created.
+    If the file already exists, its content will be overwritten.
