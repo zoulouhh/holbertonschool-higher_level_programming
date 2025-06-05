d74bbfadcc3a61e066b7d8200f12aecdde119a4d
zoulouhh <azladji.sil@gmail.com>
2025-06-05 08:47:50 +0000

    4-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This script defines a function to deserialize a JSON-formatted string
+into a native Python data structure.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Converts a JSON-formatted string into a corresponding Python object.
+
+    Args:
+        my_str (str): A string in JSON format.
+
+    Returns:
+        object: The Python data structure represented by the JSON string.
+    """
+    string_json = json.loads(my_str)
+    return string_json
