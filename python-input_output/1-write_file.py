80a775ae1a10c05dfdd5c18dc0b41640b5593850
zoulouhh <azladji.sil@gmail.com>
2025-06-05 08:42:03 +0000

    1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+Executable script using the Python 3 interpreter.
+
+This script defines a function that writes a string to a UTF-8 encoded
+text file and returns the number of characters written.
+"""
