609e804f5a34ecf05022b280ac802041fb8fb793
zoulouhh <azladji.sil@gmail.com>
2025-06-12 08:12:49 +0000

    task_02_requests.py

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,12 @@
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
