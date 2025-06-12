5cda3c6101a28d70d865e8cdaf81cc54fcc7398e
zoulouhh <azladji.sil@gmail.com>
2025-06-12 12:28:01 +0000

    task_03_http_server.py

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,16 @@
+import http.server
+import json
+import socketserver
+
+
+PORT = 8000
+
+
+class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == '/data':
