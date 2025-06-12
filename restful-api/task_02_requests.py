05d5524ab892fcd381669a227bc6e2d6479c189c
zoulouhh <azladji.sil@gmail.com>
2025-06-12 08:31:49 +0000

    task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -11,15 +11,11 @@ def fetch_and_print_posts():
         for post in posts:
             print(post['title'])
 
-            def fetch_and_save_posts():
+
+def fetch_and_save_posts():
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
 
-  if response.status_code == 200:
-        posts = response.json()
-        structured_posts = [{'id': post['id'], 'title': post['title'],
-                             'body': post['body']} for post in posts]
-
-         if response.status_code == 200:
+    if response.status_code == 200:
         posts = response.json()
         structured_posts = [{'id': post['id'], 'title': post['title'],
                              'body': post['body']} for post in posts]
