3a2054e8117cba0106809962364e8d75ec43e6fb
zoulouhh <azladji.sil@gmail.com>
2025-09-09 22:12:00 +0000

    0-square_matrix_simple.py

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []          
+        for value in row:       
+            new_row.append(valude ** 2)
+        new_matrix.append(new_row)   
+
+    return new_matrix
