93807e4ffab7329d6771bb16a224ea98ba326ce1
zoulouhh <azladji.sil@gmail.com>
2025-09-10 13:30:32 +0000

    1-search_replace.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,9 +2,8 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
-        new_row = []
+            new_row = []
         for value in row:
             new_row.append(value ** 2)
         new_matrix.append(new_row)
     return new_matrix
-    
