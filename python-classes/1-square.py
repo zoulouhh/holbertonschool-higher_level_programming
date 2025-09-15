b665319e2c8c1baeb0e4b62a63397a3de986b6c3
zoulouhh <azladji.sil@gmail.com>
2025-09-15 12:55:34 +0000

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,6 +3,7 @@
 class Square:
      # Méthode spéciale appelée lors de la création d'une instance de Square
     def __init__(self, size):
-       # Attribut d'instance privé qui stocke la taille du carré
+        # Attribut d'instance privé qui stocke la taille du carré
         # Le double underscore rend l'attribut privé (non accessible directement)
-       self.__size = size
+        self.__size = size
+
