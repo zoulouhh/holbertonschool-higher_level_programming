26fee14700803b123a91a654fdafba9305ae3ae8
zoulouhh <azladji.sil@gmail.com>
2025-09-15 09:24:00 +0000

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 class Square:
-   
-    pass
+    """
+    This class defines a square.
+
+    Attributes:
+        size (int): The size of the square's sides.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square's sides.
+        """
+        self.__size = size
