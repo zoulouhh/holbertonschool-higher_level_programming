f16eb3dc71ef4af9e50ffaa49e4bf914da1854e1
zoulouhh <azladji.sil@gmail.com>
2025-09-22 10:47:13 +0000

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module defines a base class for geometry operations."""
+
+    class BaseGeometry:
+    """A base class used for geometry-related operations."""
+
+         def area(self):
