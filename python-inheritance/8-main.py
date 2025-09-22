b1e6a2cd55045ebe1237e71ceb9191ab3aa45751
zoulouhh <azladji.sil@gmail.com>
2025-09-22 11:17:51 +0000

    all

--- python-inheritance/8-main.py
+++ python-inheritance/8-main.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
-class Rectangle(BaseGeometry):
-    """A base class for geometry operations."""
-    
+Rectangle = __import__('8-rectangle').Rectangle
+
+if __name__ == "__main__":
+    # Création d'une instance Rectangle valide
+    r1 = Rectangle(10, 5)
+    print(f"Rectangle width: {r1._Rectangle__width}")
+    print(f"Rectangle height: {r1._Rectangle__height}")
+
+    # Essai avec des valeurs invalides (doit lever une exception)
+    try:
+        r2 = Rectangle(-3, 4)
+    except Exception as e:
+        print(e)
+
+    try:
+        r3 = Rectangle(3, "4")
+    except Exception as e:
+        print(e)
