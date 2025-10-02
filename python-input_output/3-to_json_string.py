49d6f0d9f545375a8eaf87d01826a1cc67c5056e
zoulouhh <azladji.sil@gmail.com>
2025-10-02 07:57:59 +0000

    3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -6,6 +6,7 @@ Provides a function that returns the JSON representation of an object.
 
 import json
 
+
 def to_json_string(my_obj):
     """Returns the JSON representation of an object (string)"""
     return json.dumps(my_obj)
