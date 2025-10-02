6ea9f660e998851a5a1ad480be0d9460f98bcb85
zoulouhh <azladji.sil@gmail.com>
2025-10-02 07:55:36 +0000

    2-append_write.py

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 2-append_write
+Provides a function to append a string to a UTF-8 text file.
+"""
+
+def append_write(filename="", text=""):
+    """Appends a string to a text file (UTF8) and returns number of chars added"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
